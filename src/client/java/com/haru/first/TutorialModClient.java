f3f28c140845c6164010b475034e8f6164909bcc
scully06 <ppap.marisa@gmail.com>
2024-02-01 10:00:36 +0900

    Initial Commit

---
+++ src/client/java/com/haru/first/TutorialModClient.java
@@ -0,0 +1,10 @@
+package com.haru.first;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
